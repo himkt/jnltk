83d26df556bb6fa7cd8ab1dfbcbb9f139cde19e4
himkt <himkt@klis.tsukuba.ac.jp>
2017-01-25 18:19:56 +0900

    Fix the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "jnltk",
-    version = "0.0.4",
+    version = "0.0.1",
     author = 'himkt',
     author_email = 'himkt@klis.tsukuba.ac.jp',
     description = 'Japanese Natural Language Processing toolkit',
